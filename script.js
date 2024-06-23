fb0288cabde65638704e769ee718213cd22f04c6
Abhishek Lokhande <115551272+AAbhishekLokhande@users.noreply.github.com>
2024-06-23 20:48:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 //your JS code here. If required.
-	// let body = document.getElementByTagName('body');
 	let button = document.getElementById('enterBtn');
-	let para = document.getElementByTagName('p');
-	// let body = document.getElementByTagName('body');
-    button.addEventListner("click",function() {
+	let para = document.getElementById('status');
+    button.addEventListner("click", () => {
 	let h1Tag = body.createElement("h1");
-		h1Tag.innerText= "Entered Metaverse"
+	let h1Tag.innerText= "Entered Metaverse"
 	let h1Tag.id = "status"
-	// let bold = h1Tag.createElement("b");
 	
-	// let body = document.getElementByTagName('body');
-		// let para = document.getElementByTagName('p');
-	// body.appendChild('h1Tag');
-document.body.insertBefore(h1Tag,para);
+document.body.insertBefore(h1Tag , para);
 	para.remove('para');
-	
+	 
 })
