b5b08e6c16cf45b1802a948b0af020dc0068fac1
Abhishek Lokhande <115551272+AAbhishekLokhande@users.noreply.github.com>
2024-06-23 20:43:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 	// let body = document.getElementByTagName('body');
 	let button = document.getElementById('enterBtn');
 	let para = document.getElementByTagName('p');
-	let body = document.getElementByTagName('body');
+	// let body = document.getElementByTagName('body');
     button.addEventListner("click",function() {
 	let h1Tag = body.createElement("h1");
+		h1Tag.innerText= "Entered Metaverse"
 	let h1Tag.id = "status"
 	// let bold = h1Tag.createElement("b");
-	h1Tag.innerText= "Entered Metaverse"
+	
 	// let body = document.getElementByTagName('body');
 		// let para = document.getElementByTagName('p');
 	// body.appendChild('h1Tag');
